569997f90ad3443d889b208dc5f475c654afb386
Rythem <imrythem16@gmail.com>
2024-01-30 09:52:49 +0530

    Files updated

---
+++ OOP/src/Packages/Greeting.java
@@ -0,0 +1,11 @@
+package Packages;
+
+
+import static Packages.Message.message;
+
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        message();
+    }
+}
---
+++ OOP/src/StaticExample/Main.java
@@ -0,0 +1,10 @@
+package StaticExample;
+
+public class Main {
+    public static void main(String[] args) {
+        Human rythem = new Human(22,"Rythem",10000,false);
+        Human rahul = new Human(20,"Rahul",9000,true);
+        System.out.println(Human.population);
+        System.out.println(Human.population);
+    }
+}
